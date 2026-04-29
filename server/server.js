9420b2fc37e024bd5a838c790e3d847ea7aa1c16
OxygenForge-code <oxygenforge@gmail.com>
2026-04-29 17:39:45 +0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,35 +1,61 @@
 const express = require("express");
 const http = require("http");
 const socketIo = require("socket.io");
+const { createBullet, updateBullets, getBullets } = require("./bullets");
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
+// client klasörünü yayınla
 app.use(express.static("client"));
 
 let players = {};
 
+// Oyuncu bağlanınca
 io.on("connection", (socket) => {
     console.log("Oyuncu bağlandı:", socket.id);
 
     players[socket.id] = { x: 200, y: 200 };
 
+    // Hareket
     socket.on("move", (input) => {
         const speed = 5;
+
         if (input.up) players[socket.id].y -= speed;
         if (input.down) players[socket.id].y += speed;
         if (input.left) players[socket.id].x -= speed;
         if (input.right) players[socket.id].x += speed;
     });
 
+    // Ateş etme
+    socket.on("shoot", ({ dirX, dirY }) => {
+        const p = players[socket.id];
+        if (!p) return;
+
+        createBullet(p.x, p.y, dirX, dirY, socket.id);
+    });
+
+    // Çıkınca sil
     socket.on("disconnect", () => {
+        console.log("Oyuncu çıktı:", socket.id);
         delete players[socket.id];
     });
 });
 
+// Oyun loop
 setInterval(() => {
-    io.emit("state", players);
+    updateBullets();
+
+    io.emit("state", {
+        players,
+        bullets: getBullets()
+    });
 }, 50);
 
-server.listen(3000, () => console.log("Server running on 3000"));
+// 🔥 Render uyumlu PORT
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+    console.log("Server running on port " + PORT);
+});
